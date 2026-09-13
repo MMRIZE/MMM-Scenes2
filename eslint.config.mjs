cc60269ad4bec5e28c71257fe7e802c50b025762
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-09-13 17:37:17 +0200

    chore(lint): rework linter setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,44 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import css from '@eslint/css'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import markdown from '@eslint/markdown'
+import stylistic from '@stylistic/eslint-plugin'
 
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-];
+export default defineConfig([
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      sourceType: 'script',
+    },
+    extends: [js.configs.recommended, stylistic.configs.recommended],
+  },
+  {
+    files: ['**/*.mjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node,
+      },
+      sourceType: 'module',
+    },
+    extends: [js.configs.recommended, stylistic.configs.recommended],
+  },
+  {
+    files: ['**/*.css'],
+    language: 'css/css',
+    plugins: { css },
+    extends: [css.configs.recommended],
+  },
+  {
+    files: ['**/*.md'],
+    language: 'markdown/gfm',
+    plugins: { markdown },
+    extends: ['markdown/recommended'],
+  },
+])
